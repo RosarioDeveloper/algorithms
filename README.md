f66dc162af32f644a6b0f87c5b26cf7c24b9c8cd
Rosario Lourenco <rosariomassango@gmail.com>
2024-02-19 01:03:19 +0100

    Update javascript algorithms

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-## JavaScript Algotithms
+## Array Algotithms
 
-<h4>Meeting Rooms III </h4>
-You are given an integer n. There are n rooms numbered from 0 to n - 1.
-
-You are given a 2D integer array meetings where meetings[i] = [starti, endi] means that a meeting will be held during the half-closed time interval [starti, endi). All the values of starti are unique.
-
-Meetings are allocated to rooms in the following manner:
-
-Each meeting will take place in the unused room with the lowest number.
-If there are no available rooms, the meeting will be delayed until a room becomes free. The delayed meeting should have the same duration as the original meeting.
-When a room becomes unused, meetings that have an earlier original start time should be given the room.
+<ul>
+   <li><h4>Meeting Rooms III </h4></li>
+</ul>
---
+++ javascript/array.js
@@ -0,0 +1,34 @@
+/**
+ * Transpose Matrix
+    You're given a 2D array of integers <span>matrix</span>. Write a function
+    that returns the transpose of the matrix.
+
+    The transpose of a matrix is a flipped version of the original matrix across
+    its main diagonal (which runs from top-left to bottom-right); it switches
+    the row and column indices of the original matrix.
+
+    You can assume the input matrix always has at least 1 value; however its
+    width and height are not necessarily the same.
+ */
+
+function transposeMatrix(matrix) {
+   let res = [];
+   for (var arr of matrix) {
+      for (var i = 0; i < arr.length; i++) {
+         if (!res[i]) {
+            res[i] = [arr[i]];
+         } else {
+            res[i].push(arr[i]);
+         }
+      }
+   }
+
+   return res;
+}
+
+transposeMatrix([
+   [0, 4, 2, 42],
+   [-1, 5, 3, 100],
+   [-2, 6, -2, 30],
+   [-3, 7, -3, -42],
+]);
